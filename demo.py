0e090ede7b2b685cca83795204536658700ef7b0
Domanskyi <s.domanskyi@gmail.com>
2019-09-25 14:51:51 -0400

    update demo

--- demo.py
+++ demo.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     HCAtool.PrepareDataOnePatient(os.path.join('data', 'ica_bone_marrow_h5.h5'), dataName, os.path.join('data', ''), useAllData=False, cellsLimitToUse=3000)
     df_expr = pd.read_hdf(os.path.join('data', 'HCA_%s.h5'%(dataName)), key=dataName, mode='r')
 
-    DCS = DigitalCellSorter.DigitalCellSorter(nSamplesDistribution= 1000 if os.name == 'nt' else 10000, nClusters=20)
+    DCS = DigitalCellSorter.DigitalCellSorter(nSamplesDistribution=1000, nClusters=20)
     DCS.dataName = dataName
     DCS.saveDir = os.path.join('output', dataName, '')
     DCS.geneListFileName = os.path.join('geneLists', 'CIBERSORT.xlsx')
     DCS.process(df_expr)
 
-    DCSsub = DigitalCellSorter.DigitalCellSorter(dataName = dataName, nClusters = 10, doQualityControl = False, nSamplesDistribution= 1000)
+    DCSsub = DigitalCellSorter.DigitalCellSorter(dataName=dataName, nClusters=10, doQualityControl=False, nSamplesDistribution=1000)
 
     DCSsub.subclusteringName = 'T cell'
     DCSsub.saveDir = os.path.join('output', dataName, 'subclustering T cell', '')
