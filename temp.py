4b74c4d5fbc014239309d7e5a4e3961c6271454b
Domanskyi <s.domanskyi@gmail.com>
2019-10-28 10:20:21 -0400

    dev merge

--- temp.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-import DigitalCellSorter.DigitalCellSorter as DigitalCellSorter
-    
-if __name__ == '__main__':
-
-    DCS = DigitalCellSorter.DigitalCellSorter(dataName='BM1', saveDir=os.path.join('output', 'BM1', ''))
-    DCS.getAnomalyScoresPlot(cells=DCS.getCells(clusterName='Cluster #2'))
--- to_read_h5_file.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df_expr = pd.read_hdf('PBMC.h5', key='PBMC', mode='r')
-
-print(df_expr)
