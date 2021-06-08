259e63a221bb080d1c1c98596d0e00e15fbc9e05
sdomanskyi <s.domanskyi@gmail.com>
2021-06-08 16:13:34 -0400

    large demo data upload

--- docs/examples/demo.py
+++ docs/examples/demo.py
@@ -2,21 +2,37 @@ import sys
 sys.path.append("..")
 
 import os
+import urllib.request
 import DigitalCellSorter
 import DigitalCellSorter.ReadPrepareDataHCA as prep
 
 if __name__ == '__main__':
+
+    print('This is a large dataset demo.\nFor the "5k PBMC demo" run "python -m DigitalCellSorter"\n')
 
     here = os.path.dirname(__file__)
 
     url = "https://data.humancellatlas.org/project-assets/project-matrices/cc95ff89-2e68-4a08-a234-480eca21ce79.homo_sapiens.mtx.zip"
     extractPath = os.path.join(here, 'data', os.path.splitext(os.path.basename(url))[0])
 
-    # Download and unpack data
-    prep.getHCAdataByURL(url, extractPath)
+    ## Download and unpack data
+    #prep.getHCAdataByURL(url, extractPath)
 
     # Call function recordFilesOfIndividualDonors to load the data from HCA Data Portal
-    id = prep.recordFilesOfIndividualDonors(extractPath, organName='bone marrow')[0]
+    #id = prep.recordFilesOfIndividualDonors(extractPath, organName='bone marrow')[0]
+    id = '085e737d-adb5-4597-bd54-5ebeda170038'
+
+    # Get the data. The file will be downloaded from github if not found locally
+    try:
+        if not os.path.exists(extractPath):
+            os.makedirs(extractPath)
+
+        if not os.path.isfile(os.path.join(extractPath, 'dfDonorID %s.h5' % id)):
+            print('Downloading 110 Mb data file (50000 cells)')
+            urllib.request.urlretrieve('https://github.com/sdomanskyi/DigitalCellSorter/raw/master/data/dfDonorID %s.h5' % id, extractPath)
+    except Exception as exception:
+        print('Could not download the file\n', exception)
+        exit()
 
     # Load gene expression data from h5 file
     df_expr = prep.getDataframeByDonorID(extractPath, id)
@@ -24,7 +40,7 @@ if __name__ == '__main__':
 
     # Create an instance of class DigitalCellSorter. 
     # Here we use Default parameter values for most of the parameters
-    DCS = DigitalCellSorter.DigitalCellSorter(dataName='BM1', 
+    DCS = DigitalCellSorter.DigitalCellSorter(dataName='BM1', geneNamesType = 'ensembl',
                                               saveDir=os.path.join(here, 'output', 'BM1', ''),
                                               geneListFileName='CIBERSORT_LM22_7')
 
@@ -83,6 +99,3 @@ if __name__ == '__main__':
 
     # Make plots of annotated data
     DCSsub.visualize()
-
-
-
